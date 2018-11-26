30e12be5a1d8a26786ded710cb39d21121efbabd
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-11-26 09:56:03 +0000

    articleLog route added

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+const articleLogRoutes = require('./api/routes/articleLog')
+
+
 app.get('/', (req,res) => res.send({name:'HiteshGohil'}) )
+
+app.use('/article', articleLogRoutes)
 
 module.exports = app;
