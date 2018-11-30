c387415bf3a8552c5ebc27821b1002f44695a99b
hitesh-92 <hiteshgohil@hotmail.co.uk>
2018-11-30 17:07:53 +0000

    started creating MODEL column

---
+++ tests/api/models/column.test.js
@@ -0,0 +1,37 @@
+// const Column = require('../../../api/models/column')
+// const mongoose = require('mongoose')
+// const {ObjectId} = require('mongodb')
+// const assert = require('assert')
+
+
+// describe('MODEL column', ()=>{
+
+    // it('create new column', ()=>{
+
+        // const testCol = new Column({
+        //     _id: new ObjectId(),
+        //     name: 'testColumn',
+        //     lastUpdated: new Date().getTime(),
+        //     articles: [
+        //         {1: new ObjectId()},
+        //         {2: new ObjectId()},
+        //         {3: new ObjectId()}
+        //     ]
+        // })
+
+        //check data types
+        // assert.equal(typeof testCol._id, 'object')
+        // assert.equal(typeof testCol.name, 'string')
+        // assert.equal(typeof testCol.lastUpdated, 'string')
+        // assert.equal(typeof testCol.articles, 'object')
+
+        // assert.equal()
+
+    // })//
+
+// });
+
+
+// const testID = new ObjectId()
+// const check = ObjectId.isValid(testID)
+// console.log(check)
