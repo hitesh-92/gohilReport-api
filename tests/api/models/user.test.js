4f526f9120c2154e39f4488551fb8a76bda37573
hitesh-92 <hiteshgohil@hotmail.co.uk>
2019-02-01 07:30:30 +0000

    init user model and test added

---
+++ tests/api/models/user.test.js
@@ -0,0 +1,29 @@
+const User = require('../../../api/models/user')
+const mongoose = require('mongoose')
+
+const ObjectId = mongoose.Types.ObjectId
+
+// const {ObjectId} = require('mongoose')
+
+const assert = require('assert')
+
+describe.only('MODEL user', () => {
+
+    it('create new user', () => {
+
+        const usrEmail = 'user@email.com'
+        const usrPasswrd = 'go0dPassword'
+
+        const testUsr = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: usrEmail,
+            password: usrPasswrd
+        })
+
+        assert.equal(testUsr.email, usrEmail)
+        assert.equal(testUsr.password, usrPasswrd)
+        assert.equal(ObjectId.isValid(testUsr._id), true)
+
+    })
+
+});
