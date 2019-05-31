b8d9b2e9f8fc9530bbd215ec51f6e704572b11f9
hitesh <hiteshgohil@hotmail.co.uk>
2019-05-31 01:31:41 +0100

    added in correct authorisation middleware

--- api/routes/column.js
+++ api/routes/column.js
@@ -4,14 +4,11 @@ const Authenticate = require('../middleware/auth')
 const Controller = require('../controllers/column');
 const ArticleLog = require('../models/articleLog')
 const Column = require('../models/column')
-const mongoose = require('mongoose')
-const { Types: {ObjectId}} = mongoose
 
 router.get('/', (req, res) => { Controller.get_allColumns(req, res, ArticleLog, Column) });
 router.get('/single', (req, res) => { Controller.get_singleColumn(req, res, ArticleLog, Column) });
-router.post('/', (req, res) => { Controller.saveNewColumn(req, res, Column) });
-router.patch('/', (req, res) => { Controller.updateColumn(req, res, Column) });
-router.delete('/', (req, res) => { Controller.deleteColumn(req, res, Column) });
-
+router.post('/', Authenticate, (req, res) => { Controller.saveNewColumn(req, res, Column) });
+router.patch('/', Authenticate, (req, res) => { Controller.updateColumn(req, res, Column) });
+router.delete('/', Authenticate, (req, res) => { Controller.deleteColumn(req, res, Column) });
 
 module.exports = router;
