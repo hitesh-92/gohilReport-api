a2f6ee70a6806ad3b53f9620c5fa5b6a1977342e
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-08 04:10:07 +0100

    moved user login route to controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -29,4 +29,44 @@ const register = (User) => (req, res) => {
 
 }
 
-module.exports = { register }
+const login = (User) => (req, res) => {
+
+    let data = {
+        email: req.body.email,
+        loggedIn: false
+    }
+
+    const findUser = async (email, password) => {
+        const user = await User.findByCredentials(email, password)
+        if ( user === false ) return false
+        else return user
+    }
+
+    findUser( data.email, req.body.password )
+    .then( async (user) => {
+
+        if (user === false) {
+            data.message = 'Unable to find user'
+            return res.status(404).send(data)
+        }
+
+        const token = await user.createAuthToken()
+
+        if (!token) {
+            data.message = 'Unable to authenticate user'
+            res.status(401).send(data)
+        } else {
+            data.loggedIn = true
+            data.token = token;
+            res.status(200).send(data)
+        }
+    })
+    .catch(error => {
+        data.error = error
+        data.message = 'Unable to login. Contact'
+        res.status(400).send(data)
+    })
+
+}
+
+module.exports = { register, login }
--- api/routes/user.js
+++ api/routes/user.js
@@ -2,52 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 const Controller = require('../controllers/users');
-const User = require('../models/user')
-// const mongoose = require('mongoose')
-
+const User = require('../models/user');
 
 router.post('/signup', Controller.register(User) );
+router.post('/login', Controller.login(User) );
 
-router.post('/login', (req,res) => {
-
-    let data = {
-        email: req.body.email,
-        loggedIn: false
-    }
-
-    const findUser = async (email, password) => {
-        const user = await User.findByCredentials(email, password)
-        if ( user === false ) return false
-        else return user
-    }
-
-    findUser( data.email, req.body.password )
-    .then( async (user) => {
-
-        if (user === false) {
-            data.message = 'Unable to find user'
-            return res.status(404).send(data)
-        }
-
-        const token = await user.createAuthToken()
-
-        if (!token) {
-            data.message = 'Unable to authenticate user'
-            res.status(401).send(data)
-        } else {
-            data.loggedIn = true
-            data.token = token;
-            res.status(200).send(data)
-        }
-    })
-    .catch(error => {
-        data.error = error
-        data.message = 'Unable to login. Contact'
-        res.status(400).send(data)
-    })
-
-})
-
-//ADD LOGOUT
+//ADD LOGOUT => remove token
 
 module.exports = router;
