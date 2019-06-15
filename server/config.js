e1d6957dbfe4a9ec2c4a92a02292f2e5da9f7aa4
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-15 12:14:31 +0100

    users seed working locally

--- server/config.js
+++ server/config.js
@@ -3,8 +3,9 @@ var env = process.env.NODE_ENV || "development";
 if(env === 'development' || env === 'test'){
 
     var config = require('./config.json')
-    
+
     var envConfig = config[env];
+    console.log('envConfig ===> ', envConfig)
 
     Object.keys(envConfig).forEach(key => {
         process.env[key] = envConfig[key];
