30ff3b292cb23b20c60279a5bf90cd155d6df297
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-16 12:56:13 +0100

    db connection try on error

--- db/mongoose.js
+++ db/mongoose.js
@@ -10,6 +10,20 @@ const options = {
 mongoose.Promise = global.Promise;
 mongoose.connect(db_uri, options);
 
+mongoose.connection.on('connected', () =>
+  console.log('MognoDB Connected to API')
+)
+
+mongoose.connection.on('error', err => {
+  console.log('Error establishing connection with MongoDB ==> \n', err);
+  setTimeout(retryConnection, 5000);
+})
+
+function retryConnection(){
+  console.log('Attempting to re-establish connection to MongoDB');
+  return mongoose.connect(db_uri,options);
+}
+
 // mongoose.set('debug', false)
 
 module.exports = { mongoose }
