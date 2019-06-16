353cbb10557a31ffa8749f01b534e1dcd684518c
hitesh <hiteshgohil@hotmail.co.uk>
2019-06-16 09:35:54 +0100

    user schema: timestamps added. isodate

--- api/models/user.js
+++ api/models/user.js
@@ -1,113 +1,116 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
+const moment = require("moment");
 
 const UserSchema = mongoose.Schema({
-    _id: mongoose.Types.ObjectId,
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
-        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  _id: mongoose.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
     },
-    password: {
-        type: String,
-        required: true,
-        minlength: 8
-    },
-    createdAt: {
-        type: String
-    },
-    tokens: [
-        {
-            access: {
-                type: String,
-                required: true
-            },
-            token: {
-                type: String,
-                required: true
-            }
-        },
-    ]
-
-})
-
-UserSchema.pre('save', function(next){
-
-    var user = this;
-
-    const changedPassword = user.isModified('password')
-
-    const hashPassword = () => {
-        bcrypt.genSalt(1, (err, salt) => {
-            bcrypt.hash(user.password, salt, (err, hash) => {
-                user.password = hash;
-                next();
-            });
-        });
+    token: {
+      type: String,
+      required: true
     }
+  }]
+} , {
+  timestamps: true
+}
+)
 
-    if(changedPassword) hashPassword()
-    else next()
+UserSchema.pre('save', function(next) {
+
+  var user = this;
+
+  const changedPassword = user.isModified('password')
+
+  const hashPassword = () => {
+    bcrypt.genSalt(1, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    });
+  }
+
+  if (changedPassword) hashPassword()
+  else next()
 
 });
 
-UserSchema.methods.createAuthToken = function(){
+UserSchema.methods.createAuthToken = function() {
 
-    var user = this;
-    const access = 'auth'
+  var user = this;
+  const access = 'auth'
 
-    const tokenData = {
-        data: {
-            _id: user._id.toHexString(),
-            access
-        }
-    };
+  const tokenData = {
+    data: {
+      _id: user._id.toHexString(),
+      access
+    }
+  };
 
-    const token = jwt.sign(tokenData, process.env.jwtSecret, {expiresIn: '1h'}).toString()
+  const token = jwt.sign(tokenData, process.env.jwtSecret, {
+    expiresIn: '1h'
+  }).toString()
 
-    user.tokens = user.tokens.concat({access, token})
+  user.tokens = user.tokens.concat({
+    access,
+    token
+  })
 
-    return user.save().then(() => token)
+  return user.save().then(() => token)
 };
 
-UserSchema.statics.findByCredentials = function(email, password){
+UserSchema.statics.findByCredentials = function(email, password) {
 
-    var User = this;
+  var User = this;
 
-    return User.findOne({email})
+  return User.findOne({
+      email
+    })
     .exec()
     .then(user => {
-        if (!user) return Promise.reject()
+      if (!user) return Promise.reject()
 
-        return new Promise((resolve, reject) => {
-            bcrypt.compare(String(password), String(user.password))
-            .then(result => {
-                result === true ? resolve(user) : reject(false)
-            })
-        })
+      return new Promise((resolve, reject) => {
+        bcrypt.compare(String(password), String(user.password))
+          .then(result => {
+            result === true ? resolve(user) : reject(false)
+          })
+      })
     });
 };
 
-UserSchema.statics.findByToken = function(token){
-    var User = this;
-    let decoded;
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  let decoded;
 
-    try {
-        decoded = jwt.verify(token, process.env.jwtSecret)
-    }
-    catch (err)
-    {
-        return Promise.reject(err)
-    }
+  try {
+    decoded = jwt.verify(token, process.env.jwtSecret)
+  } catch (err) {
+    return Promise.reject(err)
+  }
 
-    return User.findOne({
-        '_id': decoded.data._id,
-        'tokens.access': 'auth',
-        'tokens.token': token
-    })
+  return User.findOne({
+    '_id': decoded.data._id,
+    'tokens.access': 'auth',
+    'tokens.token': token
+  })
 };
 
 
--- tests/api/models/user.test.js
+++ tests/api/models/user.test.js
@@ -3,21 +3,26 @@ const ObjectId = require('mongoose').Types.ObjectId
 
 describe('MODEL user', () => {
 
-    it('create new user', () => {
+  it('create new user', async () => {
 
-        const usrEmail = 'user@email.com'
-        const usrPasswrd = 'go0dPassword'
+    const usrEmail = 'user@email.com'
+    const usrPasswrd = 'go0dPassword'
 
-        const testUsr = new User({
-            _id: new ObjectId(),
-            email: usrEmail,
-            password: usrPasswrd
-        })
-
-        assert.equal(testUsr.email, usrEmail)
-        assert.equal(testUsr.password, usrPasswrd)
-        assert.equal(ObjectId.isValid(testUsr._id), true)
-
+    const newuser = new User({
+      _id: new ObjectId(),
+      email: usrEmail,
+      password: usrPasswrd
     })
+
+    let user = await newuser.save();
+
+    assert.equal(user.email, usrEmail);
+    assert.equal(typeof user.password, 'string');
+    assert.equal(ObjectId.isValid(user._id), true);
+    assert.equal(typeof user.createdAt, 'object');
+    assert.equal(typeof user.updatedAt, 'object');
+    assert.equal(user.tokens.length, 0)
+
+  })
 
 });
